3b7689a9ef69f2f5d8cc40dc34b4731045aa6d21
Maxwell Oliveira <maxwell1de1oliveira@gmail.com>
2022-04-04 17:26:57 -0300

    add column with remove button

--- main.js
+++ main.js
@@ -1,40 +1,6 @@
 (function(win, doc) {
   'use strict';
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
 
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
-
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
-
-  Essas informações devem ser adicionadas no HTML via Ajax.
-
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
-
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
-  
   function app(){
 
     var $nomeEmp = new DOM('[data-js="nomeEmpresa"]');
@@ -48,6 +14,8 @@
     var $submit = new DOM('[data-js="submit"]');
 
     var $table = new DOM('[data-js="table"]');
+
+    var id = 0;
     
     var addCompanyInfo = function() {
       var request = new httpClient();
@@ -82,26 +50,45 @@
       $placa.get()[0].value = "";
       $cor.get()[0].value = "";
       $img.get()[0].value = "";
-      
+
+      id += 1;
+    };
+
+    function handleClickRemoveButton(){
+      var $trToBeRemoved = doc.querySelector('[data-js="btn' + this.value +'"]').parentNode.parentNode;
+      $trToBeRemoved.parentNode.removeChild($trToBeRemoved);
+    }
+
+    function addRemoveButton(tr){
+
+      var td = doc.createElement('td');
+      var button = doc.createElement('button');
+      button.setAttribute('value', id);
+      button.setAttribute("data-js", "btn"+id);
+      button.textContent = "Remover";
+
+      button.onclick = handleClickRemoveButton;
+
+      td.appendChild(button);
+      tr.appendChild(td);
     };
 
     $submit.on('click', function() {
       var $documentFrag = doc.createDocumentFragment();
       var $tr = doc.createElement('tr');
 
-      
       createAndAppendTdTagImg($tr, $img.get()[0].value);
       createAndAppendTdTag($tr, $marca.get()[0].value);
       createAndAppendTdTag($tr, $ano.get()[0].value);
       createAndAppendTdTag($tr, $placa.get()[0].value);
       createAndAppendTdTag($tr, $cor.get()[0].value);
-  
+      addRemoveButton($tr);
+
       $documentFrag.appendChild($tr);
 
       $table.get()[0].appendChild($documentFrag);
 
       clearInput();
-
     })
 
     addCompanyInfo();
